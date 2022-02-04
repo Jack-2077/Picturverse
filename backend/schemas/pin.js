664c8e3a291e905ec5f0c57f0fa2dd05ecb30f19
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-03 19:46:04 -0800

    Add pin schema

---
+++ backend/schemas/pin.js
@@ -0,0 +1,58 @@
+export default {
+  name: 'pin',
+  title: 'Pin',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'destination',
+      title: 'destination',
+      type: 'url',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        //hotspot makes it possible to responsively adapt the images to different aspect sizes
+        hotspot: true,
+      },
+    },
+    {
+      name: 'userId',
+      title: 'UserID',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'save',
+      title: 'Save',
+      type: 'array',
+      of: [{ type: 'save' }],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [{ type: 'comment' }],
+    },
+  ],
+};
