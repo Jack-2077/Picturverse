bcc07ab17a6c3bb4fc5d0aa56fa3be543b88061c
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-04 20:08:32 -0800

    create Pins file for displaying all the images

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useRef, useRef } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import Sidebar from '../components/Sidebar';
-import UserProfile from '../components/UserProfile';
+import { Sidebar, UserProfile } from '../components/index';
+import Pins from './Pins';
+import { client } from '../Client';
 
 const Home = () => {
   return <div>Home</div>;
