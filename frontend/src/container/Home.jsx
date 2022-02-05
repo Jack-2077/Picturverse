af1caf565fd8fd4cc1de4a34d8af50ec6f3d491e
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-04 21:18:54 -0800

    get userinfo from local storage

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,12 +1,41 @@
-import React, { useState, useRef, useRef } from 'react';
+import React, { useState, useRef, useRef, useEffect } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+
+import { HiMenu } from 'react-icons/hi';
 
 import { Sidebar, UserProfile } from '../components/index';
 import Pins from './Pins';
 import { client } from '../Client';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [ToggleSidebar, setToggleSidebar] = useState(false);
+
+  const checkUserInfo = localStorage.getItem('user');
+  const userInfo =
+    checkUserInfo !== 'undefined'
+      ? JSON.parse(checkUserInfo)
+      : localStorage.clear(); //in case the token expires, we clear the local storage
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar />
+        <div className='flex md:hidden flex-row'>
+          <HiMenu
+            fontSize={40}
+            className='cursor-pointer'
+            onClick={() => setToggleSidebar(false)}
+          />
+          <Link to='/'>
+            {/* <img src={logo} alt={logo} className='w-28' /> */}
+          </Link>
+          <Link to={`user-profile/${user?._id}`}></Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
