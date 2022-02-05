3f4b7e3cb33bb724edb9a225334b066985300e5d
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-04 19:01:11 -0800

    navigate to home page on login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,7 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import bgPicture from '../assessts/pexels-jakub-novacek-924824.jpg';
 
+import { client } from '../Client';
+
 const Login = () => {
+  const navigate = useNavigate();
   const responseGoogle = (res) => {
     localStorage.setItem('user', JSON.stringify(res.profileObj));
 
@@ -16,12 +19,14 @@ const Login = () => {
       userName: name,
       image: imageUrl,
     };
+
+    client.createIfNotExists(doc).then(navigate('/', { replace: true }));
   };
+
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
         <img src={bgPicture} className='w-full h-full object-cover' />
-
         <div className='absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0 bg-blackOverlay'>
           <div className='p-5'></div>
           <div className='shadow-2xl'>
