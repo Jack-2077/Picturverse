010776fac0809ee62e9dd86c4de2ee74a935740f
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-04 21:38:06 -0800

    Add sidebar for mobile view

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -7,8 +7,11 @@ import { Sidebar, UserProfile } from '../components/index';
 import Pins from './Pins';
 import { client } from '../Client';
 
+import { useQuery } from '../utils/data';
+
 const Home = () => {
-  const [ToggleSidebar, setToggleSidebar] = useState(false);
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
 
   const checkUserInfo = localStorage.getItem('user');
   const userInfo =
@@ -16,24 +19,44 @@ const Home = () => {
       ? JSON.parse(checkUserInfo)
       : localStorage.clear(); //in case the token expires, we clear the local storage
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const query = useQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setUser[data[0]];
+    });
+  }, []);
 
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
         <Sidebar />
-        <div className='flex md:hidden flex-row'>
-          <HiMenu
-            fontSize={40}
-            className='cursor-pointer'
-            onClick={() => setToggleSidebar(false)}
-          />
-          <Link to='/'>
-            {/* <img src={logo} alt={logo} className='w-28' /> */}
-          </Link>
-          <Link to={`user-profile/${user?._id}`}></Link>
-        </div>
       </div>
+      <div className='flex md:hidden flex-row'>
+        <HiMenu
+          fontSize={40}
+          className='cursor-pointer'
+          onClick={() => setToggleSidebar(true)}
+        />
+        <Link to='/'>
+          {/* <img src={logo} alt={logo} className='w-28' /> */}
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt={logo} className='w-28' />
+        </Link>
+      </div>
+      {toggleSidebar && (
+        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+          <div className='absolute w-full flex justify-end items-center p-2'>
+            <AiFillCloseCircle
+              fontSize={30}
+              className='cursor-pointer'
+              onClick={() => setToggleSidebar(false)}
+            />
+          </div>
+          <Sidebar />
+        </div>
+      )}
     </div>
   );
 };
